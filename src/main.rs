e67e7644c978f824686a2b0962525c1d723902ed
Georg <krysopath@gmail.com>
2021-02-19 14:06:15 +0100

    add simple rocket tests

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 mod components;
-
 use crate::components::payloads::{PricingPayload};
-
 use std::collections::HashMap;
-
-use js_sandbox::{Script, AnyError};
+use js_sandbox::{
+    Script, 
+    AnyError
+};
 use serde::{Serialize};
-
 use serde_json::{
     json,
     Value,
     error::Error,
 };
-
 use rocket::{
     State,
     Data,
+    Rocket,
 };
-
 use rocket;
 use rocket_contrib::json::{JsonValue};
 
@@ -93,13 +91,69 @@ fn eval(data: Data, _key_cache: State<KeyCache>) -> JsonValue {
     JsonValue(json!(response))
 }
 
+fn build_rocket() -> Rocket {
+    rocket::ignite()
+        .mount("/", rocket::routes![index, eval])
+        .manage(KeyCache::new())
+}
+
 fn main() {
     //#[cfg(feature="static")]
     // uncomment above & build against 
     // musl lib for maximum static links
-    
-    rocket::ignite()
-        .mount("/", rocket::routes![index, eval])
-        .manage(KeyCache::new())
+    build_rocket() 
         .launch();
 }
+
+#[cfg(test)]
+mod test {
+    use super::build_rocket;
+    use rocket::local::Client;
+    use rocket::http::{
+        ContentType, 
+        Status
+    };
+
+    #[test]
+    fn hello_world_on_get() {
+        let client = Client::new(build_rocket())
+            .expect("valid rocket instance");
+
+        let mut response = client.get("/").dispatch();
+
+        assert_eq!(
+            response.status(), 
+            Status::Ok
+        );
+        assert_eq!(
+            response.body_string(), 
+            Some("Hello, world!".into())
+        );
+    }
+
+    #[test]
+    fn eval_payload_on_post() {
+        let client = Client::new(build_rocket())
+            .expect("valid rocket instance");
+
+        let mut response = client
+            .post("/")
+            .body(r#"{
+                "variables":{"A":2,"B":2},
+                "script":"return 2;",
+                "key":"59bcc3ad6775562f845953cf01624225"
+            }"#)
+            .header(ContentType::JSON)
+            .dispatch();
+
+        assert_eq!(
+            response.status(), 
+            Status::Ok
+        );
+        assert_eq!(
+            response.body_string(), 
+            Some(r#"{"status":"ok","result":{"Result":2}}"#.to_string())
+        );
+    }
+}
+
