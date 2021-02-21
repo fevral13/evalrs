cd7c378a3da45042da655c7bdae65fdd8a923225
Georg <krysopath@gmail.com>
2021-02-21 02:33:57 +0100

    intermediate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 mod components;
-use crate::components::payloads::{PricingPayload};
+use crate::components::{
+    payloads::PricingPayload,
+    validate::ecma_script,
+};
+
 use std::collections::HashMap;
 use js_sandbox::{
     Script, 
@@ -10,7 +14,7 @@ use serde::{Serialize};
 use serde_json::{
     json,
     Value,
-    error::Error,
+    error::Error as SerdeError,
 };
 use rocket::{
     State,
@@ -20,11 +24,28 @@ use rocket::{
 use rocket;
 use rocket_contrib::json::{JsonValue};
 
+use std::{
+    error::Error,
+    fmt,
+};
+
+
+#[derive(Debug, Clone)]
+pub struct ExecutionError;
+
+impl fmt::Display for ExecutionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "could not execute this script!")
+    }
+}
+
+impl Error for ExecutionError {}
+
 
 #[derive(Serialize, Debug)]
 enum CalcResult {
     Result(Value),
-    Error(String),
+    //ExecutionError(String),
 }
 
 #[derive(Serialize, Debug)]
@@ -41,10 +62,25 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+fn execute_script(script: js_sandbox::Script, variables: serde_json::Value) -> Result<CalcResult, ExecutionError> {
+    let pricing_result: Result<Value, AnyError> 
+        = script.call("wrapper", &variables);
+
+    match pricing_result {
+        Ok(price) => { 
+            Ok(CalcResult::Result(price))
+        },
+        Err(error) => {
+            Err(error.to_string())
+        },
+    }
+}
+
+
 #[rocket::post("/", data = "<data>")]
 fn eval(data: Data, _key_cache: State<KeyCache>) -> JsonValue {
 
-    let maybe_json: Result<PricingPayload, Error> 
+    let maybe_json: Result<PricingPayload, SerdeError> 
         = serde_json::de::from_reader(
             data.open()
         );
@@ -52,11 +88,11 @@ fn eval(data: Data, _key_cache: State<KeyCache>) -> JsonValue {
     let response: ResultResponse = match maybe_json {
         Ok(data)=> {
             let raw_code = &data.script;
-            let code = format!(r#"wrapper = (variables) => {{
-                   {raw_code}
-                }}"#,
+            let code = format!(r#"wrapper = (variables) => {{ {raw_code} }}"#,
                                raw_code = raw_code
             );
+
+            println!("code:\n{}", code);
 
             match Script::from_string(&code) {
                 Err(error) => ResultResponse { 
@@ -64,19 +100,7 @@ fn eval(data: Data, _key_cache: State<KeyCache>) -> JsonValue {
                     result: CalcResult::Error(error.to_string()) 
                 },
                 Ok(mut script) => {
-                    let pricing_result: Result<Value, AnyError> 
-                        = script.call("wrapper", &data.variables);
-
-                    match pricing_result {
-                        Ok(price) => ResultResponse { 
-                            status: "ok", 
-                            result: CalcResult::Result(price) 
-                        },
-                        Err(error) => ResultResponse { 
-                            status: "error", 
-                            result: CalcResult::Error(error.to_string()) 
-                        },
-                    }
+                    execute_script(script, data.variables)
                 }
             }
         },
@@ -140,7 +164,7 @@ mod test {
             .post("/")
             .body(r#"{
                 "variables":{"A":2,"B":2},
-                "script":"return 2;",
+                "script":"return variables.A + variables.B;",
                 "key":"59bcc3ad6775562f845953cf01624225"
             }"#)
             .header(ContentType::JSON)
@@ -152,7 +176,57 @@ mod test {
         );
         assert_eq!(
             response.body_string(), 
-            Some(r#"{"status":"ok","result":{"Result":2}}"#.to_string())
+            Some(r#"{"status":"ok","result":{"Result":4}}"#.to_string())
+        );
+    }
+
+    #[test]
+    fn eval_euklid_payload() {
+        let client = Client::new(build_rocket())
+            .expect("valid rocket instance");
+
+        let mut response = client
+            .post("/")
+            .body(r#"{
+                "variables":{"A":2,"B":2},
+                "script":"function euklid(a, b) {\n    if (b === 0) return a;\n    return euklid(b, a % b);\n}\nreturn euklid(12882736482, 1272)",
+                "key":"59bcc3ad6775562f845953cf01624225"
+            }"#)
+            .header(ContentType::JSON)
+            .dispatch();
+
+        assert_eq!(
+            response.body_string(), 
+            Some(r#"{"status":"ok","result":{"Result":6}}"#.to_string())
+        );
+        assert_eq!(
+            response.status(), 
+            Status::Ok
+        );
+    }
+
+    #[test]
+    fn eval_broken_payload() {
+        let client = Client::new(build_rocket())
+            .expect("valid rocket instance");
+
+        let mut response = client
+            .post("/")
+            .body(r#"{
+                "variables":{"A":2,"B":2},
+                "script":"return euklid(12882736482, 1272)",
+                "key":"59bcc3ad6775562f845953cf01624225"
+            }"#)
+            .header(ContentType::JSON)
+            .dispatch();
+
+        assert_eq!(
+            response.body_string(), 
+            Some(r#"{"status":"error","result":{"Error":"ReferenceError: euklid is not defined\n    at wrapper (sandboxed.js:1:109)\n    at sandboxed.js:2:24"}}"#.to_string())
+        );
+        assert_eq!(
+            response.status(), 
+            Status::BadRequest
         );
     }
 }
